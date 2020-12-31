c238bb9540f44a2e34afed5113058d23af560dba
SuaYoo <sua@suayoo.com>
2020-12-31 15:49:49 -0800

    add links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Root, Head, Routes, addPrefetchExcludes } from 'react-static';
-//
+
 import { Link, Router } from 'components/Router';
 import Dynamic from 'containers/Dynamic';
 
@@ -9,13 +9,67 @@ import './app.css';
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic']);
 
+function getEmailLink({ subject, body }) {
+  return `mailto:info@bestprogramming.club?subject=${window.encodeURIComponent(
+    subject
+  )}&body=${window.encodeURIComponent(body)}`;
+}
+
 function App() {
   return (
     <Root>
       <Head>
         <title>Best Programming Club</title>
       </Head>
-      <div style={{ textAlign: 'center', marginTop: '10em' }}>Coming soon</div>
+      <div style={{ textAlign: 'center', margin: '15rem 1rem 0' }}>
+        <h1>
+          BESt PROGRAMMiNG ClUb{' '}
+          <span role="img" aria-label="smiling face stuck out tongue">
+            😋
+          </span>
+        </h1>
+        <div>
+          <div style={{ fontSize: '3rem', fontWeight: 'bold' }}>2021</div>
+          <div style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>
+            <p>
+              <a
+                href={getEmailLink({
+                  subject: 'I want to sponsor BPC',
+                  body:
+                    'Include your name and details about your sponsorship interest between the lines below:\n---\n\n\n---',
+                })}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Become a sponsor
+              </a>
+            </p>
+            <p>
+              <a
+                href={getEmailLink({
+                  subject: 'I want to join BPC',
+                  body:
+                    'Include your name and details about your membership request between the lines below:\n---\n\n\n---',
+                })}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Join Discord server
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://github.com/bestprogrammingclub"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
       {/* <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
